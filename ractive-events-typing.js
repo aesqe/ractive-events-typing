57199aedb54199279241be048e511a39a98c69bd
Aesqe <aesqe@skyphe.org>
2016-11-13 21:55:19 +0100

    Refactor a bit, fix module, bump version to 0.1.3

--- ractive-events-typing.js
+++ ractive-events-typing.js
@@ -4,7 +4,7 @@ var typingEvent = (function ( global, factory ) {
 
     // Common JS (i.e. browserify) environment
     if ( typeof module !== "undefined" && module.exports && typeof require === "function" ) {
-        factory( require( "ractive" ) );
+        return factory( require( "ractive" ) );
     }
 
     // AMD?
@@ -25,114 +25,123 @@ var typingEvent = (function ( global, factory ) {
 
     "use strict";
 
-    var typing = function (node, fire) {
+    var timer = null;
+    var eligibles = ["inputtext", "input", "textareatextarea"];
+    var stopped;
+    var delay = 500;
+
+    try {
+        document.createEvent("TouchEvent");
+        delay = 1000;
+    } catch (e) {}
+
+    var startedTyping = function (event, fire) {
+        var that = this;
+
+        clearTimeout(timer);
+
+        if( stopped === undefined || stopped === true )
+        {
+            fire({
+                node: that,
+                original: event,
+                typingState: "typing",
+                sourceKey: (event.type === "paste") ? "paste" : "typed"
+            });
+        }
+
+        stopped = false;
+
+        timer = setTimeout(function () {
+            // stopped is used to stop continuous fire of state `typing`
+            stopped = true;
+            fire({
+                node: that,
+                original: event,
+                typingState: "paused"
+            });
+        }, delay);
+    };
+
+    var typedKeys = function (event, fire) {
+        // backspace or delete
+        if ([8, 46].indexOf(event.keyCode) > -1) {
+            startedTyping.call(this, event, fire);
+        }
+    };
+
+    var stoppedTyping = function (event, fire) {
+        if( timer ) {
+            clearTimeout(timer);
+        }
+
+        fire({
+            node: this,
+            original: event,
+            typingState: "stopped"
+        });
+
+        stopped = undefined;
+    };
+
+    var beforeTyping = function (event, fire) {
+        fire({
+            node: this,
+            original: event,
+            typingState: "beforetyping" // may be, need a good name for this.
+        });
+    };
+
+    var wrapEvent = function ( func, fire ) {
+        return function(event) {
+            func.call(this, event, fire);
+        };
+    };
+
+    var typingFunc = function (node, fire) {
 
         var tagName = (node.tagName || "");
         var nodeType = (node.type || "");
         var tagAndType = (tagName + nodeType).toLowerCase();
         var contentEditable = typeof node.contentEditable != "undefined";
-        var eligibles = ["inputtext", "input", "textareatextarea"];
         var eligibleForTyping = eligibles.indexOf(tagAndType) === 0 || contentEditable;
 
-        var delay = 500;
-
-        // Options
-        try {
-            document.createEvent("TouchEvent");
-            delay = 1000;
-        } catch (e) {}
+        var keypress = wrapEvent(startedTyping, fire);
+        var blur = wrapEvent(stoppedTyping, fire);
+        var focus = wrapEvent(beforeTyping, fire);
+        var keydown = wrapEvent(typedKeys, fire);
 
         if (eligibleForTyping) {
-            var typing = false,
-                timer = null,
-                stopped;
-
-            var startedTyping = function (event) {
-
-                var that = this;
-
-                clearTimeout(timer);
-
-                if( stopped === undefined || stopped === true ) {
-                    fire({
-                        node: that,
-                        original: event,
-                        typingState: "typing",
-                        sourceKey: (event.type === "paste") ? "paste" : "typed"
-                    });
-                }
-
-                stopped = false;
-
-                timer = setTimeout(function () {
-                    // stopped is used to stop continuous fire of state `typing`
-                    stopped = true;
-                    fire({
-                        node: that,
-                        original: event,
-                        typingState: "paused"
-                    });
-                }, delay);
-
-            };
-
-            var typedKeys = function (event) {
-                // backspace or delete
-                if ([8, 46].indexOf(event.keyCode) > -1) {
-                    startedTyping.call(this, event);
-                }
-            };
-
-            var stoppedTyping = function (event) {
-                if( timer ) {
-                    clearTimeout(timer);
-                }
-
-                fire({
-                    node: this,
-                    original: event,
-                    typingState: "stopped"
-                });
-
-                stopped = undefined;
-            };
-
-            var beforeTyping = function (event) {
-                fire({
-                    node: this,
-                    original: event,
-                    typingState: "beforetyping" // may be, need a good name for this.
-                });
-            };
-
-            node.addEventListener("focus", beforeTyping);
-            node.addEventListener("keypress", startedTyping);
-            node.addEventListener("keydown", typedKeys);
-            node.addEventListener("paste", startedTyping);
-            node.addEventListener("blur", stoppedTyping);
+            node.addEventListener("focus", focus);
+            node.addEventListener("keypress", keypress);
+            node.addEventListener("keydown", keydown);
+            node.addEventListener("paste", keypress);
+            node.addEventListener("blur", blur);
             // Todo : stoppedTyping when window lose focus, for now getting error from ractive.js (?)
             // window.addEventListener("blur", stoppedTyping);
         }
 
         return {
             teardown: function () {
-                node.removeEventListener("focus", beforeTyping);
-                node.removeEventListener("keypress", startedTyping);
-                node.removeEventListener("keydown", typedKeys);
-                node.removeEventListener("paste", startedTyping);
-                node.removeEventListener("blur", stoppedTyping);
-                // window.removeEventListener("blur", stoppedTyping);
+                if (eligibleForTyping) {
+                    node.removeEventListener("focus", focus);
+                    node.removeEventListener("keypress", keypress);
+                    node.removeEventListener("keydown", keydown);
+                    node.removeEventListener("paste", keypress);
+                    node.removeEventListener("blur", blur);
+                    // window.removeEventListener("blur", stoppedTyping);
+                }
             }
         };
     };
 
-    Ractive.events.typing = typing;
+    Ractive.events.typing = typingFunc;
 
-    return typing;
+    return typingFunc;
 }));
 
 // Common JS (i.e. browserify) environment
-if ( typeof module !== "undefined" && module.exports) {
+if ( typeof module !== "undefined" && module.exports ) {
     module.exports = typingEvent;
 }
 
